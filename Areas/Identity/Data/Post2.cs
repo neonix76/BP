c992f9fff8090caf97dd209ea7c09c48611afdef
neonix76 <sergeu12890@yandex.ru>
2024-11-12 23:45:56 +0300

    Добавление строки автора к информации о посте

--- Areas/Identity/Data/Post2.cs
+++ Areas/Identity/Data/Post2.cs
@@ -5,6 +5,7 @@
         public int ID { get; set; }
         public string Title { get; set; }   
         public string Text { get; set; }
+        public virtual ApplicationUser Author { get; set; }
 
     }
 }
